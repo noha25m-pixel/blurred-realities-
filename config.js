5635fa7e9c2e62b3305670bdafde31479027cb4c
noha25m-pixel <noha25m@gmail.com>
2026-07-18 16:39:58 +0300

    update

--- config.js
+++ config.js
@@ -99,8 +99,8 @@ const VIDEO_CONFIG = [
     title: "شخصية عامة حقيقية",
     description: "مقطع حقيقي من يوتيوب",
     type: "youtube",
-    youtubeId: "hCzwv9RPLkE",      // من: https://www.youtube.com/shorts/J2Rajefa25U
-    file: "celebrity_real.mp4",
+    youtubeId: "hCzwv9RPLkE",
+    file: "celebrity_real.mp4",    // من: https://www.youtube.com/shorts/J2Rajefa25U
     groundTruth: "حقيقي",
     hint: "انتبه لسياق البيئة وطبيعية التعبيرات"
   },
